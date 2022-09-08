d423a51b306b37de7e7333d34d03485a3749eddc
Emily Orr <erorr@unc.edu>
2022-09-08 16:17:16 -0400

    Progress on Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -5,38 +5,37 @@ __author__ = "730555076"
 secret_word: str = "python"
 guess_word: str = input(f"What is your {len(secret_word)}-letter guess? ")
 
-#If the guess is not the correct amount of characters
+# if the guess is not the correct amount of characters
 while len(guess_word) != len(secret_word):
-    input(f"That was not {len(secret_word)} letters! Try again: ")
+    guess_word = input(f"That was not {len(secret_word)} letters! Try again: ")
 
-#Defining index and resulting emojis variables
-index_of_word: int = 0
-emojis_of_guess: str = ""
+# defining index of secret word and resulting emojis variables
+i: int = 0
+result: str = ""
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-while index_of_word < len(secret_word):#producing green boxes
-    if guess_word[index_of_word] == secret_word[index_of_word]:
-        emojis_of_guess += GREEN_BOX
-    else:#producing white and yellow boxes
+while i < len(secret_word):# producing green boxes
+    if guess_word[i] == secret_word[i]:
+        result += GREEN_BOX
+    else:# producing white and yellow boxes
         guessed_character: bool = False
-        alternative_indices: int = 0
-        while guessed_character is not True and alternative_indices < len(secret_word):
-            if secret_word[alternative_indices] == guess_word[index_of_word]:
+        idx: int = 0
+        while guessed_character is not True and idx < len(secret_word):
+            if secret_word[idx] == guess_word[i]:
                 guessed_character = True
             else:
-                alternative_indices += 1
+                idx += 1
         if guessed_character is True:
-            emojis_of_guess += YELLOW_BOX
+            result += YELLOW_BOX
         else:
-            emojis_of_guess += WHITE_BOX
-    index_of_word += 1
-print(emojis_of_guess)
+            result += WHITE_BOX
+    i += 1
+print(result)
 
-#If correct word is guessed
 if len(guess_word) == len(secret_word):
-    if str(guess_word) == str(secret_word):
+    if str(guess_word) == str(secret_word):# when the correct word is guessed
         print("Woo! You got it!")
-    else:#if incorrect word is guessed
+    else:# when an incorrect word is guessed
         print("Not quite. Play again soon!")
